dd247591fc40ae214f4bd764455c9455e21d0dfe
Nolan Shami <nolan.shami@stu.ocsb.ca>
2022-12-16 19:28:53 +0000

    Added average_calc.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by: Nolan Shami
+# Created on: Dec 9th, 2022
+# This file contains the constants
+# used in file: average_calc.py
+
+MAX_ARRAY_SIZE = 10
+MIN_NUM = 0
+MAX_NUM = 100
